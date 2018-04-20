9f531098fb1eb34eb3236e88711240d7e3804895
Konstantin Varlamov <varconst@google.com>
2018-04-20 15:57:10 -0400

    compilation wip

--- Firestore/core/src/firebase/firestore/util/async_queue_libdispatch.h
+++ Firestore/core/src/firebase/firestore/util/async_queue_libdispatch.h
@@ -25,145 +25,75 @@
 #include <vector>
 
 #include "absl/strings/string_view.h"
-
-/** An implementation of `AsyncQueue` built on top of libdispatch. */
+#include "Firestore/core/src/firebase/firestore/util/firebase_assert.h"
 
 namespace firebase {
 namespace firestore {
 namespace util {
 
+namespace {
+
+absl::string_view StringViewFromLabel(const char* const label) {
+  // Make sure string_view's data is not null, because it's used for logging.
+  return label ? absl::string_view{label} : absl::string_view{""};
+}
+
+}  // namespace
+
+// DelayedOperationImpl
+
 namespace internal {
 
-class AsyncQueueImpl;
-class DelayedOperationImpl;
+template <typename Tag> class Executor;
+using Operation = std::function<void()>;
+using Milliseconds = std::chrono::milliseconds;
 
-}  // namespace internal
-
-/**
- * Handle to an operation scheduled via AsyncQueue::EnqueueAfterDelay. Supports
- * cancellation via the cancel method.
- */
-class DelayedOperation {
+// All member functions, including the constructor, are *only* invoked on the
+// Firestore queue. The only exception is `operator<`.
+template <typename Tag>
+class DelayedOperationImpl {
  public:
-  /**
-   * Cancels the operation if it hasn't already been executed or canceled.
-   *
-   * As long as the operation has not yet been run, calling `Cancel()` (from an
-   * operation already running on the dispatch queue) provides a guarantee that
-   * the operation will not be run.
-   */
+DelayedOperationImpl(Executor<Tag>* const executor,
+                       const Milliseconds delay,
+                       Operation&& operation)
+      : executor_{executor}, target_time_{
+            std::chrono::time_point_cast<Milliseconds>(
+                std::chrono::system_clock::now()) +
+            delay},
+        operation_{std::move(operation)} {
+  }
+
   void Cancel();
+  void RescheduleAsap();
+  bool operator<(const DelayedOperationImpl& rhs) const {
+    return target_time_ < rhs.target_time_;
+  }
 
-  DelayedOperation() {
+  void MarkDone() {
+    done_ = true;
   }
 
  private:
-  // Don't allow callers to create their own valid `DelayedOperation`s.
-  friend class internal::AsyncQueueImpl;
-  explicit DelayedOperation(
-      const std::shared_ptr<internal::DelayedOperationImpl>& operation)
-      : handle_{operation} {
-  }
+  static void InvokedByLibdispatch(void* const raw_self);
+  void Execute();
+  void Dequeue();
 
-  std::weak_ptr<internal::DelayedOperationImpl> handle_;
+  using TimePoint = std::chrono::time_point<std::chrono::system_clock,
+                                            Milliseconds>;
+
+  Executor<Tag>* const executor_;
+  const TimePoint target_time_;  // Used for sorting
+  const Operation operation_;
+
+  // True if the operation has either been run or canceled.
+  //
+  // Note on thread-safety: `done_` is only ever accessed from `Cancel` and
+  // `Execute` member functions, and both of them are only ever invoked by the
+  // dispatch queue, which provides synchronization.
+  bool done_ = false;
 };
 
-class AsyncQueue {
- public:
-  using Milliseconds = std::chrono::milliseconds;
-  using Operation = std::function<void()>;
-
-  explicit AsyncQueue(const dispatch_queue_t dispatch_queue);
-
-  /**
-   * Asserts that we are already running on this queue (actually, we can only
-   * verify that the queue's label is the same, but hopefully that's good
-   * enough).
-   */
-  void VerifyIsCurrentQueue() const;
-
-  /**
-   * Declares that we are already executing on the correct `dispatch_queue_t`
-   * and would like to officially execute code on behalf of this `AsyncQueue`.
-   * To be used only when called back by some other API directly onto our queue.
-   * This allows us to safely dispatch directly onto the worker queue without
-   * destroying the invariants this class helps us maintain.
-   */
-  void EnterCheckedOperation(const Operation& operation);
-
-  /**
-   * Same as `dispatch_async()` except it asserts that we're not already on the
-   * queue, since this generally indicates a bug (and can lead to re-ordering of
-   * operations, etc).
-   *
-   * @param operation The operation to run.
-   */
-  void Enqueue(const Operation& operation);
-
-  /**
-   * Unlike `Enqueue`, this method does not require you to dispatch to a
-   * different queue than the current one (thus it is equivalent to a raw
-   * `dispatch_async()`).
-   *
-   * This is useful, e.g. for dispatching to the user's queue directly from user
-   * API call (in which case we don't know if we're already on the user's queue
-   * or not).
-   *
-   * @param operation The operation to run.
-   */
-  void EnqueueAllowingSameQueue(const Operation& operation);
-
-  /**
-   * Schedules an operation after the specified delay.
-   *
-   * Unlike `Enqueue`, this method does not require you to dispatch to a
-   * different queue than the current one.
-   *
-   * The returned `DelayedOperation` handle can be used to cancel the operation
-   * prior to its running.
-   *
-   * @param delay The delay after which to run the operation.
-   * @param timer_id A `TimerId` that is used as a tag to identify which caller
-   *     has scheduled this operation. For each value of `TimerId`, only
-   *     a single operation tagged with that value can be in the queue at any
-   *     given moment; an attempt to schedule a second one will result in an
-   *     error. The `TimerId` is mostly intended to be used from tests to check
-   *     for the presence of this operation or to schedule it to run early.
-   * @param operation The operation to run.
-   * @return A `DelayedOperation` instance that can be used for cancellation.
-   */
-  DelayedOperation EnqueueAfterDelay(Milliseconds delay,
-                                     TimerId timer_id,
-                                     Operation operation);
-
-  /**
-   * Wrapper for `dispatch_sync()`. Mostly meant for use in tests.
-   *
-   * @param operation The operation to run.
-   */
-  void RunSync(const Operation& operation);
-
-  /**
-   * For tests: determine if a delayed operation with a particular `TimerId`
-   * exists.
-   */
-  bool ContainsDelayedOperation(TimerId timer_id) const;
-
-  /**
-   * For tests: runs delayed operations early, blocking until completion.
-   *
-   * @param last_timer_id Only delayed operations up to and including one that
-   *     was scheduled using this `TimerId` will be run. Method crashes if no
-   *     matching operation exists.
-   */
-  void RunDelayedOperationsUntil(TimerId last_timer_id);
-
-  /** The underlying wrapped `dispatch_queue_t`. */
-  dispatch_queue_t dispatch_queue() const;
-
- private:
-  std::shared_ptr<internal::AsyncQueueImpl> impl_;
-};
+// Executor
 
 // Generic wrapper over dispatch_async_f, providing dispatch_async-like
 // interface: accepts an arbitrary invocable object in place of an Objective-C
@@ -173,10 +103,10 @@ void DispatchAsync(const dispatch_queue_t queue, Work&& work) {
   // Wrap the passed invocable object into a std::function. It's dynamically
   // allocated to make sure the object is valid by the time libdispatch gets to
   // it.
-  const auto wrap = new AsyncQueue::Operation(std::forward<Work>(work));
+  const auto wrap = new Operation(std::forward<Work>(work));
 
   dispatch_async_f(queue, wrap, [](void* const raw_operation) {
-    const auto unwrap = static_cast<AsyncQueue::Operation*>(raw_operation);
+    const auto unwrap = static_cast<Operation*>(raw_operation);
     (*unwrap)();
     delete unwrap;
   });
@@ -187,14 +117,156 @@ template <typename Work>
 void DispatchSync(const dispatch_queue_t queue, Work&& work) {
   // Unlike dispatch_async_f, dispatch_sync_f blocks until the work passed to it
   // is done, so passing a pointer to a local variable is okay.
-  AsyncQueue::Operation wrap{std::forward<Work>(work)};
+  Operation wrap{std::forward<Work>(work)};
 
   dispatch_sync_f(queue, &wrap, [](void* const raw_operation) {
-    const auto unwrap = static_cast<AsyncQueue::Operation*>(raw_operation);
+    const auto unwrap = static_cast<Operation*>(raw_operation);
     (*unwrap)();
   });
 }
 
+
+template <typename Tag>
+class Executor {
+ public:
+  explicit Executor(const dispatch_queue_t dispatch_queue)
+      : dispatch_queue_{dispatch_queue} {
+  }
+  Executor()
+      : Executor{dispatch_queue_create("com.google.firebase.firestore",
+                                       DISPATCH_QUEUE_SERIAL)} {
+  }
+
+  ~Executor() {
+    // Turn any operations that might still be in the queue into no-ops, lest
+    // they try to access `Executor` after it gets destroyed.
+    ExecuteBlocking([this] {
+      for (auto operation : operations_) {
+        operation->MarkDone();
+      }
+    });
+  }
+
+  bool IsAsyncCall() const {
+    return GetCurrentQueueLabel().data() == GetTargetQueueLabel().data();
+  }
+  absl::string_view GetInvokerId() const {
+    return GetCurrentQueueLabel();
+  }
+
+  void Execute(Operation operation) {
+    DispatchAsync(dispatch_queue(), std::move(operation));
+  }
+  void ExecuteBlocking(Operation operation) {
+    DispatchSync(dispatch_queue(), std::move(operation));
+  }
+
+  DelayedOperationImpl<Tag>* ScheduleExecution(Milliseconds delay,
+                                     Tag tag,
+                                     Operation operation) {
+    namespace chr = std::chrono;
+    const dispatch_time_t delay_ns = dispatch_time(
+        DISPATCH_TIME_NOW, chr::duration_cast<chr::nanoseconds>(delay).count());
+
+    // Ownership is fully transferred to libdispatch -- because it's impossible
+    // to truly cancel work after it's been dispatched, libdispatch is
+    // guaranteed to outlive Executor, and it's possible for work to be invoked
+    // by libdispatch after Executor is destroyed. Executor only stores an
+    // observer pointer to the operation.
+    //
+    // Invariant: if Executor contains the plain pointer to an operation, it
+    // hasn't been run or canceled yet. When libdispatch invokes the operation,
+    // it will remove the operation from Executor, and since it happens inside
+    // `dispatch` invocation, it happens-before any access from Executor to
+    // `operations_`.
+
+    auto const delayed_operation =
+        new DelayedOperationImpl<Tag>{this, delay, std::move(operation)};
+    dispatch_after_f(delay_ns, dispatch_queue(), delayed_operation,
+                     DelayedOperationImpl<Tag>::InvokedByLibdispatch);
+    operations_.push_back({tag, delayed_operation});
+    return delayed_operation;
+  }
+
+  void Remove(const DelayedOperationImpl<Tag>& to_remove) {
+    const auto found = std::find_if(operations_.begin(), operations_.end(),
+                                    [&to_remove](const TaggedOperation& op) {
+                                      return op.operation == &to_remove;
+                                    });
+    // It's possible for the operation to be missing if libdispatch gets to run
+    // it after it was force-run, for example.
+    if (found != operations_.end()) {
+      operations_.erase(found);
+    }
+  }
+
+ private:
+  dispatch_queue_t dispatch_queue() const {
+    return dispatch_queue_;
+  }
+
+  // GetLabel functions are guaranteed to never return a "null" string_view
+  // (i.e. data() != nullptr).
+  absl::string_view GetCurrentQueueLabel() const {
+    // Note: dispatch_queue_get_label may return nullptr if the queue wasn't
+    // initialized with a label.
+    return StringViewFromLabel(
+        dispatch_queue_get_label(DISPATCH_CURRENT_QUEUE_LABEL));
+  }
+
+  absl::string_view GetTargetQueueLabel() const {
+    return StringViewFromLabel(dispatch_queue_get_label(dispatch_queue()));
+  }
+
+  std::atomic<dispatch_queue_t> dispatch_queue_;
+  struct TaggedOperation {
+    Tag tag{};
+    DelayedOperationImpl<Tag>* op_{};
+  };
+  std::vector<TaggedOperation> operations_;
+};
+
+template <typename Tag>
+void DelayedOperationImpl<Tag>::Cancel() {
+  if (!done_) {
+    Dequeue();
+  }
+}
+
+template <typename Tag>
+void DelayedOperationImpl<Tag>::RescheduleAsap() {
+  FIREBASE_ASSERT_MESSAGE(!done_, "TODO");
+  executor_->Enqueue([this] { Execute(); });
+}
+
+template <typename Tag>
+void DelayedOperationImpl<Tag>::InvokedByLibdispatch(void* const raw_self) {
+  auto const self = static_cast<DelayedOperationImpl*>(raw_self);
+  self->executor_->StartExecution([self] { self->Execute(); });
+  // StartExecution is a blocking operation.
+  delete self;
+}
+
+template <typename Tag>
+void DelayedOperationImpl<Tag>::Execute() {
+  if (done_) {
+    return;
+  }
+
+  Dequeue();
+  MarkDone();
+
+  FIREBASE_ASSERT_MESSAGE(
+      operation_, "DelayedOperationImpl contains invalid function object");
+  operation_();
+}
+
+template <typename Tag>
+void DelayedOperationImpl<Tag>::Dequeue() {
+  executor_->Remove(*this);
+}
+
+}  // namespace internal
 }  // namespace util
 }  // namespace firestore
 }  // namespace firebase
